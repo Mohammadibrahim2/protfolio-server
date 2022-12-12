8f12f44e43c1a84296efdca4b8045b10eb80e8ec
Zahid Hasan Akram <Mohammadibrahim2>
2022-12-12 12:56:55 +0600

    server site

---
+++ index.js
@@ -0,0 +1,26 @@
+const express=require("express")
+const app=express()
+const cors=require("cors")
+app.use(cors())
+app.use(express.json())
+const port=process.env.PORT || 5000
+const aboutproject=require("./projects.json")
+
+const courses=require("./projects.json");
+
+
+
+   app.get("/subprojects/:id", (req,res)=>{
+    const id =req.params.id
+    const project=aboutproject.find(num=>num.id===id)
+          
+            res.send( project)
+            })
+
+  
+
+
+app.listen(port,()=>{
+    console.log("port-5000",port)
+})
+
